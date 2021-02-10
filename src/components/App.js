7184fc0c505bff50a09cda5ed4f99aff2d8ed272
suraj_122 <122suraj0@gmail.com>
2021-02-10 22:19:39 +0530

    done single post

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,23 @@
-import { Route } from 'react-router-dom';
-import Header from './Header';
-import Home from './Home';
-import Login from './Login';
-import Signup from './Signup';
+import { Route } from "react-router-dom";
+import Header from "./Header";
+import Home from "./Home";
+import Login from "./Login";
+import Signup from "./Signup";
+import SinglePost from "./SinglePost";
 function App(props) {
   return (
     <>
       <Header />
-      <Route path='/' exact>
+      <Route path="/" exact>
         <Home />
       </Route>
-      <Route path='/signup'>
+      <Route path="/singlepost">
+        <SinglePost />
+      </Route>
+      <Route path="/signup">
         <Signup />
       </Route>
-      <Route path='/login'>
+      <Route path="/login">
         <Login />
       </Route>
     </>
